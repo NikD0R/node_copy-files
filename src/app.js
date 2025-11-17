a7d4e1a29dda256f90f03548ff5b418babddf362
Nikita Doroshenko <nikitokd05@gmail.com>
2025-11-17 18:43:58 +0200

    test solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+if (process.argv.slice(2).length !== 2) {
+  /* eslint-disable-next-line no-console */
+  console.error('Provide source and destination');
+  return;
+}
+
+const [file, dest] = process.argv.slice(2);
+
+if (path.resolve(file) !== path.resolve(dest)) {
+  fs.stat(file, (error, stats) => {
+    if (error) {
+      /* eslint-disable-next-line no-console */
+      console.error(error);
+
+      return;
+    }
+
+    if (!stats.isFile()) {
+      /* eslint-disable-next-line no-console */
+      console.error('Source is not a file');
+
+      return;
+    }
+
+    fs.cp(file, dest, (err) => {
+      if (err) {
+        /* eslint-disable-next-line no-console */
+        console.error(err);
+      }
+    });
+  });
+}
